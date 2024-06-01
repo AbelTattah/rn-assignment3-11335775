43dd2de15308fe41bbab5a20616c0d29cc3a2255
Abel Tattah <122866505+AbelTattah@users.noreply.github.com>
2024-06-01 11:43:59 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  name:{
+    color:'white',
+    fontSize:24,
+    fontWeight:'bold'
+  },
+  description:{
+    color:'white',
+    fontSize:24,
+  }
+});
